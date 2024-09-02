e1440e80914ead7243e0d2ef1fae3821564ec1c2
Yousuf <yousufahmed64@outlook.com>
2024-09-02 08:03:31 +0300

    Initial commit

---
+++ Application/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace DemoProject02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IEnumerable<int> numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        PrintArray("Numbers", numbers);
+        PrintArray("Even numbers", numbers.Where(x => IsEven(x)));
+        PrintArray("Odd numbers", numbers.Where(x => IsOdd(x)));
+    }
+    static void PrintArray(string title, IEnumerable<int> numbers)
+    {
+        Console.Write($"{title}: [");
+        foreach (int i in numbers)
+        {
+            Console.Write($" {i}");
+        }
+        Console.Write(" ]");
+        Console.WriteLine();
+    }
+    static bool IsEven(int value)
+    {
+        if(value % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    static bool IsOdd(int value)
+    {
+        if (value % 2 != 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
